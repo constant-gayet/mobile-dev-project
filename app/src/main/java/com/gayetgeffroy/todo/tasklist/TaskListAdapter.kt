ad2da74d955d851536e6a4442b1e44418cb145c5
constant-gayet <76485344+constant-gayet@users.noreply.github.com>
2022-11-28 14:35:16 +0100

    commit tp1-2

--- app/src/main/java/com/gayetgeffroy/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/gayetgeffroy/todo/tasklist/TaskListAdapter.kt
@@ -1,18 +1,29 @@
 package com.gayetgeffroy.todo.tasklist
 
 import android.app.ActivityManager
+import android.content.ClipData
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gayetgeffroy.todo.R
 
 
-// l'IDE va râler ici car on a pas encore implémenté les méthodes nécessaires
-class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+object TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task) : Boolean {
+        return // comparaison: est-ce la même "entité" ? => même id?
+    }
 
-    var currentList: List<Task> = emptyList()
+    override fun areContentsTheSame(oldItem: Task, newItem: Task) : Boolean {
+        return // comparaison: est-ce le même "contenu" ? => mêmes valeurs? (avec data class: simple égalité)
+    }
+}
+
+// l'IDE va râler ici car on a pas encore implémenté les méthodes nécessaires
+class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(TaskDiffCallback) {
 
     // on utilise `inner` ici afin d'avoir accès aux propriétés de l'adapter directement
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -23,8 +34,8 @@ class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
         fun bind(taskTitle: String, taskDescription: String) {
             // on affichera les données ici
-            titleView.text = taskTitle
             descriptionView.text = taskDescription
+            titleView.text = taskTitle
 
         }
     }
@@ -38,7 +49,5 @@ class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
             holder.bind(currentList[position].title, currentList[position].description)
     }
 
-    override fun getItemCount(): Int {
-    return currentList.size
-    }
+
 }
