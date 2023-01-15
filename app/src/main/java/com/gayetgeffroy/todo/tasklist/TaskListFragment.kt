289b942a5da96f15a051bcc11e2c10c944306d6f
Barnabé Geffroy <barnabe.geffroy@ecomail.fr>
2023-01-15 16:39:38 +0100

    intégration todoist fonctionnelle

--- app/src/main/java/com/gayetgeffroy/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/gayetgeffroy/todo/tasklist/TaskListFragment.kt
@@ -2,8 +2,10 @@ package com.gayetgeffroy.todo.tasklist
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -61,6 +63,22 @@ class TaskListFragment : Fragment() {
             viewModel.edit(task)
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //if (savedInstanceState != null) {
+        //    this.taskList = savedInstanceState.getSerializable("taskList") as List<Task>
+        //    viewModel.edit(task)
+        //}
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root;
+    }
+
+
 
     override fun onResume() {
         super.onResume()
