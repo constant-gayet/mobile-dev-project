b6caa2eae477c1912a9b1f3eeb8faa7ea80a67b1
constant-gayet <gayet.constant@gmail.com>
2023-01-15 18:54:59 +0100

    picking photo working but nor loading

--- app/src/main/java/com/gayetgeffroy/todo/user/UserActivity.kt
+++ app/src/main/java/com/gayetgeffroy/todo/user/UserActivity.kt
@@ -3,9 +3,11 @@ package com.gayetgeffroy.todo.user
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.compose.foundation.layout.Column
@@ -20,17 +22,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import com.gayetgeffroy.todo.user.ui.theme.TodoGayetGeffroyTheme
+import okhttp3.MultipartBody
 
 
 class UserActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { result ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (result != null) {
+//                uri = result
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
+        }
         setContent {
             var bitmap: Bitmap? by remember { mutableStateOf(null) }
             var uri: Uri? by remember { mutableStateOf(null) }
             val takePicture = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
                 bitmap = it
             }
+
+            // Registers a photo picker activity launcher in single-select mode.
             TodoGayetGeffroyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -50,7 +64,9 @@ class UserActivity : ComponentActivity() {
                             content = { Text("Take picture") }
                         )
                         Button(
-                            onClick = {},
+                            onClick = {// Launch the photo picker and allow the user to choose only images.
+                               pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                                      },
                             content = { Text("Pick photo") }
                         )
                     }
